0f743eab48f2bfa8bc5d0c0ca3e719fb960efc67
Fawzi Gharib <fawz11gharib@gmail.com>
2025-03-24 21:58:50 +0300

    string operators

--- src/Main.kt
+++ src/Main.kt
@@ -1,23 +1,15 @@
+import java.util.*
+
 fun main(){
-
-
-// In Kotlin, null safety is achieved through nullable and non-nullable types like any other language.
-
-// Non-nullable type
-var nonNullable: String = "Hello, World!"
-// nonNullable = null // This will cause a compilation error
-
-// Nullable type
-var nullable: String? = "Hello, Fawzi!"
-nullable = null // This is allowed
-
-// Safe call operator (?.) - returns null if the object is null
-val length: Int? = nullable?.length
-println(length) // Output: in case of null it will print null
-
-// Elvis operator (?:) - provides a default value if the object is null
-val lengthOrZero: Int = nullable?.length ?: 0
-println(lengthOrZero)
-
-
+    var myString:String = "Hello World!"
+    println(myString.uppercase()) // HELLO WORLD!
+    println(myString.lowercase()) // hello world!
+    println(myString.replaceFirstChar { it.lowercase(Locale.getDefault()) }) // hello World!
+    println(myString.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }) // Hello world!
+    println(myString.length) // 12
+    println(myString.get(0)) // H
+    println(myString[0]) // H
+    println(myString.substring(6)) // World!
+    println(myString.substring(6, 11)) // World
+    print("$myString: ${myString.length}")
 }
