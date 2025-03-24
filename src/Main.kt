b86062431f561218f908ffd91e8070c4a86e145e
Fawzi Gharib <fawz11gharib@gmail.com>
2025-03-24 22:50:45 +0300

    data type casting

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,9 @@
 import java.util.*
 
 fun main(){
-    var myString:String = "Hello World!"
-    println(myString.uppercase()) // HELLO WORLD!
-    println(myString.lowercase()) // hello world!
-    println(myString.replaceFirstChar { it.lowercase(Locale.getDefault()) }) // hello World!
-    println(myString.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }) // Hello world!
-    println(myString.length) // 12
-    println(myString.get(0)) // H
-    println(myString[0]) // H
-    println(myString.substring(6)) // World!
-    println(myString.substring(6, 11)) // World
-    print("$myString: ${myString.length}")
+   var finalExamScore:String = "4.7 GPA";
+
+   var result:Double? = finalExamScore.toDoubleOrNull()?.plus(1.0);
+
+    println("The final exam score is: $result");
 }
