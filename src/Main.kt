6d3cf9bfddde1b39b0065bd16f9478d4c174953c
Fawzi Gharib <fawz11gharib@gmail.com>
2025-03-24 23:02:38 +0300

    implement basic arithmetic operations with user input

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,39 @@
 import java.util.*
 
 fun main(){
-   var finalExamScore:String = "4.7 GPA";
+    val scanner = Scanner(System.`in`)
+    println("Enter two integers: ")
+    val num1 = scanner.nextInt()
+    val num2 = scanner.nextInt()
+    scanner.close()
 
-   var result:Double? = finalExamScore.toDoubleOrNull()?.plus(1.0);
 
-    println("The final exam score is: $result");
+    print("Addition: ")
+    addition(num1, num2)
+    print("Division: ")
+    division(num1, num2)
+    print("Multiplication: ")
+    multiplication(num1, num2)
+    print("Subtraction: ")
+    subtraction(num1, num2)
+}
+
+
+fun addition(x:Int,y:Int){
+    println(x + y)
+}
+
+fun division(x:Int,y:Int){
+    if(y == 0){
+        throw IllegalArgumentException("Division by zero is not allowed")
+    }
+    println(x / y)
+}
+
+fun multiplication(x:Int,y:Int){
+    println(x * y)
+}
+
+fun subtraction(x:Int,y:Int){
+    println(x - y)
 }
