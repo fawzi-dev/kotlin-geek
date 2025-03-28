4d51aab87ba2e02da21dcd154a42fa134bdf1ab5
Fawzi Gharib <fawz11gharib@gmail.com>
2025-03-28 23:02:24 +0300

    fix loop breaking logic in multiplication table

--- src/Main.kt
+++ src/Main.kt
@@ -8,7 +8,7 @@ fun main() {
    parentLoop@ for (i in 1..10) {
       childLoop@  for (j in 1..10) {
           // Since we can name loops, we can break a single loop
-          if(i*j==35) break@childLoop;
+          if(i*j==35) break@parentLoop;
             print("${i * j}\t")
         }
         println()
