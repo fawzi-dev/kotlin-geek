b0494520c0932908f83b115c9ad7042b461949dc
Fawzi Gharib <fawz11gharib@gmail.com>
2025-03-28 22:42:46 +0300

    refactor main function to use a result variable for range evaluation output

--- src/Main.kt
+++ src/Main.kt
@@ -1,13 +1,17 @@
 import java.util.*
 
-fun main(){
-val number = 25
+fun main() {
+    val number = 19
 
-if (number in 10..20) {
-    println("Number is between 10 and 20")
-} else if (number in 21..30) {
-    println("Number is between 21 and 30")
-} else {
-    println("Number is outside the specified ranges")
-}
+
+    val result:String = if (number in 10..20) {
+        "Number is between 10 and 20"
+    } else if (number in 21..30) {
+        "Number is between 21 and 30"
+    } else {
+        "Number is outside the specified ranges"
+    }
+
+    println(result)
+
 }
