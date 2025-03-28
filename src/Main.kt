5504c647d93daeab665564737a28bc77bd22e58a
Fawzi Gharib <fawz11gharib@gmail.com>
2025-03-28 22:58:55 +0300

    refactor main function to use when expression for number range evaluation

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,14 @@
 import java.util.*
 
 fun main() {
-    when (val number:Int = -1) {
-        in 1..10 -> println("The number $number is between 1 and 10")
-        in 11..20 -> println("The number $number is between 11 and 20")
-        else -> println("The number $number is outside the range 1 to 20")
+
+    /// Simple multiplication table
+
+    for (i in 1..10) {
+        for (j in 1..10) {
+            print("${i * j}\t")
+        }
+        println()
     }
 
 }
